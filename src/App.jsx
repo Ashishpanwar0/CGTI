1fbcac4dfff5963d429fa42e36c819e7584c5fc0
Ashish Panwar <ashupanwar5684@gmail.com>
2025-09-08 08:11:14 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css'
+import Home from './Pages/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>
+  },
+
+  {
+    
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+);
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Navbar from '../Componants/Navbar.jsx';
+
+export default function Home() {
+  return (
+ <>
+        <Navbar/>
+        <h1>hellow</h1>
+ </>
+  );
+}
