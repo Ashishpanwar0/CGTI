0dca76dc0e64a4a5e900e8249655f185e915f9d9
Ashish Panwar <ashupanwar5684@gmail.com>
2025-09-10 08:45:46 +0530

    First commit

--- src/Componants/Carousel.jsx
+++ src/Componants/Carousel.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Carousel = ({}) => {
     return(
         <>
-            
+            <h1>helwo wrd</h1>
+            <p>lerm
+
+            </p>
+            <input type="text" />
+            <input type="text" />
         </>
     )
 }
