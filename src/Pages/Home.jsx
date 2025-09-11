913b4e62d4dbdab097b2b6d66772b83c41fd997d
Ashish Panwar <ashupanwar5684@gmail.com>
2025-09-11 21:05:38 +0530

    First commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Componants/Navbar";
+import Carousel from "../Componants/Carousel";
 
 const Home = ({}) => {
   return (
     <>
       <Navbar />
+      <Carousel/>
     </>
   );
 };
