7a56317cf8886a7e66190fd6c89310621f53fbc0
Ashish Panwar <ashupanwar5684@gmail.com>
2025-09-11 21:30:37 +0530

    First commit

--- src/Componants/Carousel.jsx
+++ src/Componants/Carousel.jsx
@@ -1,38 +1,39 @@
 import React from 'react';
+import 'flowbite';
 
 const Carousel = ({}) => {
     return(
-        <>
-            
+        <section>
+        
 
-<div id="animation-carousel" class="relative w-full" data-carousel="static">
+<div id="animation-carousel" className="relative w-full" data-carousel="static">
     <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-        <div class="hidden duration-200 ease-linear" data-carousel-item>
-           <img src="https://computerguru.co.in/wp-content/uploads/2023/10/Computer-Guru-Training-Institute-banner-2.jpg" alt="" />
+        <div className="hidden duration-200 ease-linear" data-carousel-item>
+            <img src="http://computerguru.co.in/wp-content/uploads/2023/10/Computer-Guru-Training-Institute-banner-2.jpg" alt="" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"/>
         </div>
-        <div class="hidden duration-200 ease-linear" data-carousel-item>
-           <img src="https://computerguru.co.in/wp-content/uploads/2023/10/Computer-Guru-Training-Institute-banner-3.jpg" alt="" />
+        <div className="hidden duration-200 ease-linear" data-carousel-item>
+            <img src="https://computerguru.co.in/wp-content/uploads/2023/10/Computer-Guru-Training-Institute-banner-3.jpg" alt="" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"/>
         </div>
     </div>
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+    <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
             </svg>
-            <span class="sr-only">Previous</span>
+            <span className="sr-only">Previous</span>
         </span>
     </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+    <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
             <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
             </svg>
-            <span class="sr-only">Next</span>
+            <span className="sr-only">Next</span>
         </span>
     </button>
 </div>
 
-        </>
+        </section>
     )
 }
 
