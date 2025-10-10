8872f2ebe7749a240e47ea2ba896f9784e3d35d7
Monu Rajput <monurajput@example.com>
2025-10-10 09:46:58 +0530

    update

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -3,12 +3,52 @@ import Navbar from "../Componants/Navbar";
 import Footer from "../Componants/Footer";
 import SecondHeader from "../Componants/SecondHeader";
 import CGTICourses from  "../assets/CGTI Courses.jpg"
+const CoursesList = [
+{
+Image: m,
+title : "Advance Diploma in Computer Application (ADCA)"
+},
+{
+Image: m,
+title : "Advance Diploma in Computer Application & Programming (ADCAP)"
+},
+{
+Image: m,
+title : "Advance Diploma in Desktop Publishing / Graphic Designing (ADDP)"
+},
+{
+Image: m,
+title : "Advance Diploma in Website Designing (ADWD)"
+},
+{
+Image: m,
+title : "Diploma In Financial Accounting (DFA)"
+},
+{
+Image: m,
+title : "Certificate in Computer Application (CCA)"
+},
+{
+Image: m,
+title : "Computer Teacher Training Course (CTTC)"
+},
+{
+Image: m,
+title : "Certificate in Data Entry Operator"
+},
+{
+Image: m,
+title : "Typing: Hindi/English"
+}
+]
 function Courses() {
   return(
      <>
       <Navbar/>
       <SecondHeader bgImage={CGTICourses} title="Courses " subtitle="Home . Courses" />
+      <section>
 
+      </section>
       <Footer/>
     </>
   );
