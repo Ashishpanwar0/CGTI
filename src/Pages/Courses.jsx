51422d573d2c24f4b9f84e30bc059bf368408f3c
Monu Rajput <monurajput@example.com>
2025-10-10 09:08:03 +0530

    Update

--- src/Pages/Courses.jsx
+++ src/Pages/Courses.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "../Componants/Navbar";
 import Footer from "../Componants/Footer";
 import SecondHeader from "../Componants/SecondHeader";
-
+import CGTICourses from  "../assets/CGTI Courses.jpg"
 function Courses() {
   return(
      <>
       <Navbar/>
-      <SecondHeader/>
+      <SecondHeader bgImage={CGTICourses} title="Courses " subtitle="Home . Courses" />
+
       <Footer/>
     </>
   );
